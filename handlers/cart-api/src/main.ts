5f1bae4577f4e3c03a948ad0cd224897f309cc0f
Mark Karnaukh <marik13021992@gmail.com>
2023-07-10 15:28:21 +0200

    Implement changes for task 8.1

---
+++ handlers/cart-api/src/main.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { INestApplication } from '@nestjs/common';
+import helmet from 'helmet';
+import { Express } from 'express';
+import { AppModule } from './app.module';
+
+// const port = process.env.PORT || 4000;
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: (req, callback) => callback(null, true),
+//   });
+//   app.use(helmet());
+
+//   await app.listen(port);
+// }
+// bootstrap().then(() => {
+//   console.log('App is running on %s port', port);
+// });
+
+export async function createApp(
+  expressApp: Express,
+): Promise<INestApplication> {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+
+  return app;
+}
--- lib/nodejs-aws-cart-api-stack.ts
+++ lib/nodejs-aws-cart-api-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NodejsAwsCartApiStack extends cdk.Stack {
@@ -8,9 +10,43 @@ export class NodejsAwsCartApiStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NodejsAwsCartApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaLayer = new lambda.LayerVersion(this, 'CartApiBackendLayer', {
+      code: lambda.Code.fromAsset('handlers/cart-api/node_modules'),
+      compatibleRuntimes: [
+        lambda.Runtime.NODEJS_16_X,
+        lambda.Runtime.NODEJS_14_X,
+      ],
+    });
+
+    const backendLambda = new lambda.Function(this, 'CartApiBackendLambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('handlers/cart-api/dist'),
+      handler: 'index.handler',
+      layers: [lambdaLayer],
+      environment: {
+        NODE_PATH: '$NODE_PATH:/opt',
+      },
+    });
+
+    new apigateway.LambdaRestApi(this, 'CartAPI', {
+      handler: backendLambda,
+      description: 'Cart Service REST API',
+      deployOptions: {
+        stageName: 'dev', // Deployment stages: 'dev' or 'prod'. By default, the stageName is set to prod.
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          ...apigateway.Cors.DEFAULT_HEADERS,
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowMethods: apigateway.Cors.ALL_METHODS, // 'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'
+        allowCredentials: true,
+        allowOrigins: apigateway.Cors.ALL_ORIGINS, // 'http://localhost:3000'
+      },
+    });
   }
 }
